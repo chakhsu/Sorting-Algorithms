ddc93b503f14826e7dba2ba322a44efe90136d64
Chakhsu Lau <zexu.liu@live.cn>
2017-09-14 10:24:10 +0800

    add bubbleSort & SelectionSort Algo examples

---
+++ bubbleSort/js/AlgoFrame.js
@@ -0,0 +1,41 @@
+class AlgoFrame {
+
+    constructor(g2d, title, canvasWidth, canvasHeight) {
+        this.g2d = g2d;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+    }
+
+    getCanvasWidth() {
+        return this.canvasWidth;
+    }
+
+    getCanvasHeight() {
+        return this.canvasHeight;
+    }
+
+    repaint() {
+        // 具体绘制
+        this.g2d.clearRect(0, 0, this.canvasWidth, this.canvasHeight+20);
+
+        var w = this.canvasWidth / this.data.N();
+
+        for (var i = 0; i < this.data.N(); i++) {
+            if (i < this.data.orderedIndex) {
+                AlgoVisHelper.setColor(this.g2d, AlgoVisHelper.Red);
+            } else {
+                AlgoVisHelper.setColor(this.g2d, AlgoVisHelper.Grey);
+            }
+            if (i === this.data.currentCompareIndex) {
+                AlgoVisHelper.setColor(this.g2d, AlgoVisHelper.LightBlue);
+            }
+            AlgoVisHelper.fillRectangle(this.g2d, i * w, this.canvasHeight - this.data.get(i) + 20, w - 1, this.data.get(i));
+            AlgoVisHelper.setText(this.g2d, this.data.get(i), i * w, this.canvasHeight - this.data.get(i) + 15);
+        }
+    }
+
+    render(data) {
+        this.data = data;
+        this.repaint();
+    }
+}
---
+++ bubbleSort/js/AlgoVisualizer.js
@@ -0,0 +1,55 @@
+class AlgoVisualizer {
+    constructor(g2d, sceneWidth, sceneHeight, N) {
+
+        // 动画的执行速度[毫秒]
+        this.DELAY = 10;
+
+        this.g2d = g2d;
+
+        // 初始化数据
+        this.data = new SelectionSortData(N, sceneHeight);
+
+        // 动画整个存储
+        this.data_list = new Array();
+
+        // 初始化视图
+        this.frame = new AlgoFrame(g2d, "Selection Sort Visualization", sceneWidth, sceneHeight);
+        this.run();
+    }
+
+    // 生成数据逻辑
+    run() {
+        this.setData(0, -1);
+
+        for (var i = 0; i < this.data.N(); i++) {
+            this.setData(i, -1);
+            for (var j = 0; j < this.data.N() - i - 1; j++) {
+                // 由小到大
+                if (this.data.get(j) > this.data.get(j+1)) {
+                    this.data.swap(j, j+1);
+                }
+            }
+        }
+        this.setData(this.data.N(), -1);
+        // 渲染数据
+        this.render();
+    }
+
+    setData(orderedIndex, currentCompareIndex) {
+        var data = new SelectionSortData();
+        data.orderedIndex = orderedIndex;
+        data.currentCompareIndex = currentCompareIndex;
+        data.numbers = this.data.numbers.slice();
+        this.data_list[this.data_list.length] = data;
+    }
+
+    render() {
+        var i = 0;
+        setInterval(() => {
+            if (i < this.data_list.length) {
+                this.frame.render(this.data_list[i]);
+                i++;
+            }
+        }, this.DELAY);
+    }
+}
